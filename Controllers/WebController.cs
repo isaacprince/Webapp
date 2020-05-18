e52524e8779f31f82124ad365043223ac3d64fdd
isaacprince <isaacprince002@gmail.com>
2020-05-18 15:30:23 +0100

    changes done

--- Controllers/WebController.cs
+++ Controllers/WebController.cs
@@ -10,39 +10,29 @@ namespace Webapp.Controllers {
         }
 
         [HttpPost]
-        public IActionResult SquareRoot (string firstNumber, string secondNumber) {
-            double numberOne = double.Parse (firstNumber);
-            double numberTwo = double.Parse (secondNumber);
-
-            double result1 = Math.Sqrt (double.Parse (firstNumber));
-            double result2 = Math.Sqrt (double.Parse (secondNumber));
-
-            ViewBag.first = result1;
-            ViewBag.second = result2;
-            ViewBag.one = numberOne;
-            ViewBag.two = numberTwo;
-
-            if (ViewBag.first != null && ViewBag.second != null) {
-
-                try {
-                    if (ViewBag.first > ViewBag.second) {
-
-                        Console.WriteLine ($"The number {ViewBag.one} with Square root {ViewBag.result1} has a higher square root than {ViewBag.two} with square root {ViewBag.result2}");
-
-                    } else if (ViewBag.second > ViewBag.first) {
-
-                        Console.WriteLine ($"The number {ViewBag.two} with Square root {ViewBag.result2} has a higher square root than {ViewBag.one} with square root {ViewBag.result1}");
-
-                    } else if (ViewBag.first == ViewBag.Second && ViewBag.second == ViewBag.one) {
-
-                        Console.WriteLine ($"The number { ViewBag.one } with Square root { ViewBag.result1 } has the same Value with { ViewBag.two } with square root {ViewBag.result2} PLEASE ENTER DIFFERENT VALUES");
-                    }
-                } catch (FormatException) {
-                    Console.WriteLine ($"WRONG INPUT PLEASE ENTER A POSITIVE VALUE!!");
+        public IActionResult SquareRoot (string FirstNumber, string SecondNumber) {
+            try {
+                if (int.Parse (FirstNumber) < 0 || int.Parse (SecondNumber) < 0) {
+                    ViewBag.NegNumError = "Please input a Postive Value";
                 }
+                double numberOne = double.Parse (FirstNumber);
+                double numberTwo = double.Parse (SecondNumber);
 
+                double result1 = Math.Sqrt (numberOne);
+                double result2 = Math.Sqrt (numberTwo);
+
+                ViewBag.one = numberOne;
+                ViewBag.two = numberTwo;
+                ViewBag.first = result1;
+                ViewBag.second = result2;
+
+            } catch (FormatException ex) {
+                string alphabetError = ex.Message;
+                ViewBag.alphaError = "please input numbers only, try again";
+            } catch (ArgumentException ex) {
+                string emptyValueError = ex.Message;
+                ViewBag.errorMessageEmpty = "Please input a number and try again";
             }
-
             return View ();
         }
     }
